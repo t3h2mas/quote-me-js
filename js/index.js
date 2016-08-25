2d75b100fa8efe2fb4c355ecf3551a209d693e57
Thomas <jtruant@gmx.com>
2016-08-24 23:00:59 -0600

    httpsify

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 function getQuote(cb) {
-  $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
+  $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
     // You can only resize so much. Let's not.
     if (a[0].content.length > 225)
       return getQuote();
     $(".text").html(a[0].content);
     $(".author").html(a[0].title);
-    
+
     cb();
   });
 }
